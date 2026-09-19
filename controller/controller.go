e7821751fa50fef3b96f5227e79c7cb853fac8d1
GamerKhaan <128838084+GamerKhaan@users.noreply.github.com>
2026-09-19 17:48:14 +0000

    chore(release): bump node to v1.0.4

--- controller/controller.go
+++ controller/controller.go
@@ -19,7 +19,7 @@ import (
 	"github.com/pasarguard/node/pkg/sysstats"
 )
 
-const NodeVersion = "1.0.3"
+const NodeVersion = "1.0.4"
 const NodeProductVersion = "0.5.4-awg31.1"
 
 type Service interface {
